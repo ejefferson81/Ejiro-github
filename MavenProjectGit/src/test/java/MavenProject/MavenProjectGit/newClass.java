d875e5e3baa2513f31b0aaeea119cc6227c17f1d
ejiroghenejefferson <ejiro1012@me.com>
2017-10-21 16:08:05 -0400

    testing 2

--- MavenProjectGit/src/test/java/MavenProject/MavenProjectGit/newClass.java
+++ MavenProjectGit/src/test/java/MavenProject/MavenProjectGit/newClass.java
@@ -1,19 +1,20 @@
 package MavenProject.MavenProjectGit;
 
-import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.server.handler.GetTitle;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 public class newClass {
+	public static Logger logger = Logger.getLogger(newClass.class);
+			
 	@Test
 	public void intializeDriver() {
 		System.setProperty("webdriver.chrome.driver", "/Users/ejiroghenejefferson/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
-
+		logger.info("Chrome is opened");
 		if (driver.getTitle().equals("Google")) {
 			System.out.println("Test Passed");
 		} else {
@@ -26,5 +27,6 @@ public class newClass {
 	System.out.println(color);
 	
 	}
+	
 
 }
